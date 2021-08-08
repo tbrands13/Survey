cc467df9ae7eb214ec3baee4c0304a3f30e38871
Titus Brandy <tbrands13@gmail.com>
2021-08-08 13:49:17 -0500

    INIT COMMIT

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Survey.Models;
+
+namespace Survey.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("submit")]
+        public IActionResult Submit(Form newUser)
+        {
+            Console.WriteLine(newUser);
+            return RedirectToAction("results", newUser);
+        }
+
+        [HttpGet("results")]
+        public IActionResult Results(Form newUser)
+        {
+
+            return View(newUser);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Form.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Survey.Models
+{
+    public class Form
+    {
+        // Name
+        public string Name {get;set;}
+        // Location
+        public string Location {get;set;}
+        // Language
+        public string Language {get;set;}
+        // Comment
+        public string Comment {get;set;}
+    }
+
+}
